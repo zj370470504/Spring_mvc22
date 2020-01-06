aaa410614f891f21afe7eb16b47d6dab858d7785
zj370470504 <370470504@qq.com>
2020-01-06 10:19:31 +0800

    123

--- src/main/java/com/bailiban/controller/HelloController.java
+++ src/main/java/com/bailiban/controller/HelloController.java
@@ -1,12 +1,14 @@
 package com.bailiban.controller;
 
+import com.bailiban.model.User;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
 @RestController
 public class HelloController {
-    @RequestMapping({"/", "", "/hello"})
-    public String hello() {
-        return "Hello World!";
+    @RequestMapping("hello")
+    public User hello(User user ) {
+        return user;
     }
 
 }
